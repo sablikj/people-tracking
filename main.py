56a14b01e409c5d011ec005458e4418621ed4c6f
Jan Sáblík <j_sablik@utb.cz>
2023-04-11 15:39:18 +0100

    Removed optical flow usage, not updating the histograms

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-import math
 import pandas as pd
 import numpy as np
 from scipy.optimize import linear_sum_assignment
@@ -9,8 +8,7 @@ show_gt = False
 minIOU = 0.3
 thr = 50 # Used for background subtraction
 minArea = 175 # Minimal area to be considered a component
-maxDistance = 25 #Centroid:25  # Maximal distance between frames of each person
-allViews = False
+maxDistance = 75 #Centroid:25  # Maximal distance between frames of each person
 kernel = np.ones((5,5),np.uint8) # Used for orphological operations
 
 # Initialize variables for tracking
@@ -18,14 +16,9 @@ tracks = []  # List of tracks, each track is a dict containing the ID, bounding 
 next_id = 1  # ID to assign to the next detected pedestrian
 max_lost_frames = 5  # Maximum number of frames a track can be lost before it is removed
 
-
-id = 1 # ID of detected person
 frame = 1 # Number of current frame
 ground_truth = pd.read_csv('./gt.txt', sep=',', names=["Frame", "ID", "bbLeft", "bbTop", "Width", "Height", "Confidence", "x", "y", "z"])
 cap = cv2.VideoCapture("./dataset/frame_%04d.jpg")
-
-# Frame, ID, bbLeft, bbTop, Width, Height, Confidence,x,y,z
-data = []
 
 ############################################################################################
 def getBackground(cap, n=25):
@@ -56,6 +49,7 @@ def getBackground(cap, n=25):
     
     return bg
 
+
 def subtractBackground(img, bg):
     """
     Subtracts image background from current frame to obtain foreground.
@@ -77,40 +71,11 @@ def subtractBackground(img, bg):
 
     return diff
 
+
 def getCentroid(box):
     x, y, w, h = box
     return (int(x+(w/2)), int(y+(h/2)))
 
-def find_matching_row(data, obj):
-    for i, row in enumerate(data):
-        if row[2:6] == obj[2:6]:
-            return i
-    return -1  # return -1 if no matching row is found
-
-def predictPosition(prev, curr, roi):
-    """
-    Uses OpticalFlow to predict position of each pixel of ROI in next frame.
-
-    Args:
-        prev (np.array): First 8-bit single-channel input image.
-        img (np.array): Next second input image of the same size and the same type as prev.
-        roi (tuple): Bounding box coordinates (x,y,w,h).
-
-    Returns:
-        tuple: Updated bounding box coordinates (x,y,w,h).
-    """
-    x,y,w,h = roi
-    prev = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-    curr = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                
-    # Apply an optical flow algorithm to calculate the motion vectors
-    flow = cv2.calcOpticalFlowFarneback(prev, curr, None, 0.5, 2, 15, 2, 5, 1.1, 0)
-
-    # Track the ROI using optical flow
-    new_x = x + int(round(flow[y:y+h, x:x+w, 0].mean()))
-    new_y = y + int(round(flow[y:y+h, x:x+w, 1].mean()))
-
-    return new_x, new_y, w, h
 
 def compute_iou(bbox1, bbox2):
     """
@@ -154,25 +119,22 @@ def compute_iou(bbox1, bbox2):
 
     return iou
 
-def update_track(track, x, y, w, h, descriptor, centroid):
+
+def update_track(track, bbox, centroid):
     """
     Update the state of a track based on a matched pedestrian in the current frame.
 
     Args:
         track (dict): A dictionary representing the track to be updated.
-        x (int): The x-coordinate of the top-left corner of the bounding box of the matched pedestrian.
-        y (int): The y-coordinate of the top-left corner of the bounding box of the matched pedestrian.
-        w (int): The width of the bounding box of the matched pedestrian.
-        h (int): The height of the bounding box of the matched pedestrian.
-        descriptor (ndarray): An array representing the descriptor of the matched pedestrian.
+        bbox (tuple): Coordinates of the bounding box.
         centroid (tuple): A tuple representing the centroid of the matched pedestrian in the format (x, y).
 
     Returns:
         None
     """
     # Update the bounding box, descriptor, and centroid of the track
-    track['bbox'] = (x, y, w, h)
-    track['descriptor'] = descriptor
+    track['bbox'] = bbox
+    #track['descriptor'] = descriptor
     track['centroid'] = centroid
     
     # Compute the velocity of the track
@@ -184,60 +146,40 @@ def update_track(track, x, y, w, h, descriptor, centroid):
     # Reset the lost frames counter
     track['lost_frames'] = 0
 
-def compute_distance(descriptor, track_desc, pos, track_bbox):
+
+def compute_distance(descriptor, track_desc):
     """
-    Compute the distance between a pedestrian in the current frame and a track.
+    Computes euclidian distance between a pedestrian in the current frame and a track.
 
     Parameters:
-        descriptor (ndarray): An array representing the descriptor of the pedestrian in the current frame.
-        track_desc (ndarray): An array representing the descriptor of the track.
-        pos (tuple): A tuple representing the position of the pedestrian in the current frame in the format (x, y, w, h).
-        track_bbox (tuple): A tuple representing the bounding box of the track in the format (x, y, w, h).
+        descriptor (ndarray): An array representing the histogram of the pedestrian in the current frame.
+        track_desc (ndarray): An array representing the histogram of the track.
 
     Returns:
         float: The distance between the pedestrian and the track.
     """
-    # Euclidean distance between the descriptor of the pedestrian and the descriptor of the track
-    desc_dist = np.linalg.norm(track_desc-descriptor)
-    print(desc_dist)
-    # Euclidean distance between the position of the pedestrian and the position of the track
-    c1 = getCentroid(pos)
-    c2 = getCentroid(track_bbox)
-    pos_dist = np.sqrt((c1[0] - c2[0])**2 + (c1[1] - c2[1])**2)
-    
-    # Total distance with added weights
-    dist = desc_dist + pos_dist
-
-    return pos_dist    
+    dist = np.linalg.norm(track_desc-descriptor)
+    return dist     
 
 
 ############################################################################################
 bg = getBackground(cap)
-prev_frame = None
 while cap.isOpened():  
-    ret, img = cap.read()
-    img_track = img.copy()
+    ret, img = cap.read()    
     if not ret:
         break
+    img_track = img.copy()
     
     # 2) OBJECT DETECTION
-    
     # Getting foreground objects -> pedestrians
     img_diff = subtractBackground(img, bg)
-
-    if allViews:
-        cv2.imshow("1. Subtracted image", img_diff)
     
     # Using closing to fill the holes in objs    
     img_cls = cv2.morphologyEx(img_diff, cv2.MORPH_CLOSE, kernel)
-    if allViews:
-        cv2.imshow('2. Applied closing', img_cls)
 
     # Using erosion to remove noise
     img_opn = cv2.erode(img_cls, kernel)
-    img_opn = cv2.dilate(img_opn, kernel)
-    if allViews:
-        cv2.imshow("3. Applied erd+dil", img_opn)
+    img_opn = cv2.dilate(img_opn, kernel)   
 
     # Running CCA alg - using connectivity 4
     num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(img_opn, connectivity=8) 
@@ -246,8 +188,7 @@ while cap.isOpened():
     filtered_labels = np.where(np.isin(labels, np.where(stats[:, cv2.CC_STAT_AREA] >= minArea)[0]), labels, 0)
     filtered_label_stats = np.where(stats[:, cv2.CC_STAT_AREA] >= minArea)[0]
     
-    img_detect = img.copy()
-    # Initialize an empty list to store the detections
+    # Initialize an empty list to store the detections in current frame
     detections = []
 
     # Iterating in detected pedestrians
@@ -261,13 +202,15 @@ while cap.isOpened():
 
         centroid = (int(x+(w/2)), int(y+(h/2)))       
 
-        # Frame, ID, bbLeft, bbTop, Width, Height, Confidence, x, y, z
-        data.append([frame, id, x, y, w, h, 0, 0, 0, 0])                 
-
+        # Remove bg from pedestrian bbox with mask from CCA
+        ped = cv2.bitwise_and(img[y:y+h, x:x+w], img[y:y+h, x:x+w], mask=img_opn[y:y+h, x:x+w])
+        
         # Compute the descriptor of the pedestrian
-        descriptor = cv2.calcHist([img[x:x+w, y:y+h]], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-        # Normalize histogram
-        descriptor = cv2.normalize(descriptor, descriptor).flatten()
+                                    # used channels, no mask, 8 bins, 0-255 val range
+        descriptor = cv2.calcHist(ped, [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+
+        # Normalize the histogram
+        #descriptor = cv2.normalize(descriptor, descriptor).flatten()
 
         # Add the pedestrian information to the detections list
         detections.append({'bbox': (x, y, w, h), 'descriptor': descriptor, 'centroid': centroid})
@@ -278,14 +221,14 @@ while cap.isOpened():
 
             for t, track in enumerate(tracks):
                 for d, det in enumerate(detections):
-                    distance = compute_distance(det['descriptor'], track['descriptor'], det['bbox'], track['bbox'])
+                    distance = compute_distance(det['descriptor'], track['descriptor'])
                     cost_matrix[t, d] = distance
 
             row_ind, col_ind = linear_sum_assignment(cost_matrix)
 
             for r, c in zip(row_ind, col_ind):
                 if cost_matrix[r, c] < maxDistance:
-                    update_track(tracks[r], detections[c]['bbox'][0], detections[c]['bbox'][1], detections[c]['bbox'][2], detections[c]['bbox'][3], detections[c]['descriptor'], detections[c]['centroid'])
+                    update_track(tracks[r], detections[c]['bbox'], detections[c]['centroid'])
                 else:
                     # Add a new track for the current pedestrian
                     track = {'id': next_id,
@@ -321,12 +264,12 @@ while cap.isOpened():
             x, y, w, h = track['bbox']
             cv2.rectangle(img_track, (x, y), (x+w, y+h), (0, 255, 0), 1)
             cv2.putText(img_track, f"{track['id']}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-        cv2.imshow('Tracked image', img_track)
         
+        cv2.putText(img_track, f"Frame: {frame}", (5,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        cv2.imshow('Tracked image', img_track)
          
     #####################################
     frame += 1
-    prev_frame = img.copy()
     # Wait for a key press to exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
